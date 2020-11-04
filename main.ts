0f350049bc8b87b7ee8b57264e504f1a9c783112
ayuai456 <71751267+ayuai456@users.noreply.github.com>
2020-11-04 14:29:11 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 basic.forever(function () {
-    for (let Y = 0; Y <= 4; Y++) {
-        for (let X = 0; X <= Y; X++) {
-            if (Y > X) {
-                led.plot(4 - X, 4 - Y)
-                led.plot(4 - Y, 4 - X)
-                basic.pause(1000)
-            } else if (Y == X) {
-                led.plot(4 - X, 4 - Y)
-                basic.pause(1000)
-            }
+    for (let Y = 0; Y <= 8; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(4 - X, 8 - (Y - X))
         }
+        basic.pause(100)
+    }
+    for (let Y = 0; Y <= 8; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(0, 0)
+        }
+        basic.pause(100)
     }
 })
